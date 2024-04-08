c929fa6d07b452b70e9af5989807b2d91fffd660
anish8865 <157658878+anish8865@users.noreply.github.com>
2024-04-08 13:23:32 -0400

    Update question-3-answers.sql

--- sql/question-3-answers.sql
+++ sql/question-3-answers.sql
@@ -1,16 +1,24 @@
--- select by id 1:
+SELECT * FROM posts WHERE id = 1;
 
 
--- select all posts where title = includes "title 2":
+
+SELECT * FROM posts WHERE title LIKE '%title 2%';
 
 
--- select all posts and order by the title column alphabetically:
+
+SELECT * FROM posts ORDER BY title ASC;
 
 
--- insert 3 new posts
+
+INSERT INTO posts (title, description) VALUES
+('New Post Title 1', 'New post description 1'),
+('New Post Title 2', 'New post description 2'),
+('New Post Title 3', 'New post description 3');
 
 
--- update posts where id = 1, set the title to "new title"
+
+UPDATE posts SET title = 'Updated Post' WHERE id = 1;
 
 
--- delete post where id = 2
+
+DELETE FROM posts WHERE id = 2;
